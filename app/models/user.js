734e7dbd43f8352d62bd67354ebd7669cc551c95
Hack Reactor Students <accounts@hackreactor.com>
2016-10-13 17:23:04 -0700

    Implement signing up new users and logging them in
    - in the style of Mongoose data storage

--- app/models/user.js
+++ app/models/user.js
@@ -9,16 +9,16 @@ var mongoose = require('mongoose');
 //   initialize: function() {
 //     this.on('creating', this.hashPassword);
 //   },
-//   comparePassword: function(attemptedPassword, callback) {
-//     bcrypt.compare(attemptedPassword, this.get('password'), function(err, isMatch) {
-//       callback(isMatch);
-//     });
-//   },
+  // comparePassword: function(attemptedPassword, callback) {
+  //   bcrypt.compare(attemptedPassword, this.get('password'), function(err, isMatch) {
+  //     callback(isMatch);
+  //   });
+  // },
 //   hashPassword: function() {
-//     var cipher = Promise.promisify(bcrypt.hash);
-//     return cipher(this.get('password'), null, null).bind(this)
-//       .then(function(hash) {
-//         this.set('password', hash);
+    // var cipher = Promise.promisify(bcrypt.hash);
+    // return cipher(this.get('password'), null, null).bind(this)
+    //   .then(function(hash) {
+    //     this.set('password', hash);
 //       });
 //   }
 // });
@@ -30,5 +30,19 @@ var userSchema = mongoose.Schema({
 });
 
 var User = mongoose.model('User', userSchema);
+
+User.prototype.comparePassword = function(attemptedPassword, callback) {
+  bcrypt.compare(attemptedPassword, this.password, function(err, isMatch) {
+    callback(isMatch);
+  });
+},
+userSchema.pre('save', function(next) {
+  var cipher = Promise.promisify(bcrypt.hash);
+  return cipher(this.password, null, null).bind(this)
+    .then(function(hash) {
+      this.password = hash;
+      next();
+    });
+});
 
 module.exports = User;
